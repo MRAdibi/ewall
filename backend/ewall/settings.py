2863398a0419e7dd653250a76913795fdc9b3266
Minerva <rezaiealireza884@gmail>
2021-04-27 09:16:31 +0430

    1 - restructred the project | 2 - added frontend(reactjs) and backend(django)

--- backend/ewall/settings.py
+++ backend/ewall/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wgeuh84egd%7a!0eg)c%%30rhu4jduakzht%fgh(_wqwdde=z*'
+SECRET_KEY = 'django-insecure-a30&a@30q*ygfpy%6szinc$=pmom&%%en=qj#&nbti&mb&gjz)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
