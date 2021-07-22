85f4b35a4e09f585dfb318675ad74236f2862603
meraj bighamian <=>
2021-07-22 21:38:40 +0430

    create model Post

--- backend/ewall/post/models.py
+++ backend/ewall/post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField('عنوان',max_length=200)
+    detail = models.TextField('توضیحات',default='توضیحاتی وجود ندارد!')
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    contact_details  = author.name
+    # covers = connect to covers table
+    cities = (
+        (1,'بوشهر'),
+        (2,'اصفهان'),
+        (3,'تهران'),
+    )
+
+    city = models.IntegerField('شهر شما : ', choices=cities)
+    description = models.CharField(max_length=200,default=detail[:201]) #not show to user 
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField('نمایش داده شود؟')
+
+
+    
