f51f0cb2b1d9e2d731a24beb277fd783375b4200
Minerva <rezaiealireza884@gmail>
2021-07-28 01:16:42 +0430

    added axios and react as frontend dependency and some backend changes

--- backend/ewall/ewall/settings.py
+++ backend/ewall/ewall/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ewall',
-        'USER': 'ewall',
-        'PASSWORD': 'meraj',
+        'USER': 'postgres',
+        'PASSWORD': '09397713369',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
